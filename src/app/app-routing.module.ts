c81af1a76522a44801795c2cbde5e2ce5cdf2ea4
ClaytonPrebelli <clayton@prebelli.online>
2022-01-02 18:49:26 -0300

    Criado modulo categories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { CategoriesModule } from './pages/categories/categories.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'categories',
+    loadChildren: () => CategoriesModule
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
