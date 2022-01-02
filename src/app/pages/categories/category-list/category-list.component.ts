70469b9a9c1abc6af6edfb341206bc4589ab331a
ClaytonPrebelli <clayton@prebelli.online>
2022-01-02 20:36:59 -0300

    criado list pelo array

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,7 @@
+import { Category } from './../shared/category.model';
+import { CategoryService } from './../shared/category.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+
+    this.categoryService.getAll().subscribe(categories => this.categories = categories,
+      error => alert('Erro ao carregar a lista'));
   }
 
+  deleteCategory(id: number) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+
+    if (mustDelete) {
+      this.categoryService.delete(id).subscribe(() => this.categories = this.categories.filter(c => c.id != id));
+    }
+  }
 }
